5c636fc5b0538e248c9da36b6a273470154aa64d
ChiaraGiangiulli <chiara.giangiulli@studio.unibo.it>
2022-10-29 17:19:19 +0200

    finished 64-design-with-maps

--- 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
@@ -35,7 +35,9 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      *
      * In order to save the people followed by a user organized in groups, adopt
      * a generic-type Map:  think of what type of keys and values would best suit the requirements
-     */
+     */ 
+    private Map<String, Set<U>> groups;
+    final static int DEFAULT_AGE = -1;
 
     /*
      * [CONSTRUCTORS]
@@ -62,13 +64,17 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      *            application
      */
     public SocialNetworkUserImpl(final String name, final String surname, final String user, final int userAge) {
-        super(null, null, null, 0);
+        super(name, surname, user, userAge);
+        this.groups = new HashMap<>();
     }
-
+    
     /*
      * 2) Define a further constructor where the age defaults to -1
      */
-
+    public SocialNetworkUserImpl(final String name, final String surname, final String user) {
+            super(name, surname, user, DEFAULT_AGE);
+            this.groups = new HashMap<>();
+    }
     /*
      * [METHODS]
      *
@@ -76,7 +82,12 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      */
     @Override
     public boolean addFollowedUser(final String circle, final U user) {
-        return false;
+        Set<U> followers = this.groups.get(circle);
+        if(followers == null){
+            followers = new HashSet<>();
+            this.groups.put(circle, followers);
+        }
+        return followers.add(user);
     }
 
     /**
@@ -86,11 +97,22 @@ public final class SocialNetworkUserImpl<U extends User> extends UserImpl implem
      */
     @Override
     public Collection<U> getFollowedUsersInGroup(final String groupName) {
-        return null;
+        Collection<U> users = this.groups.get(groupName);
+        if (users != null){
+            return new ArrayList<>(users);
+        }
+        else{
+            return Collections.emptyList();
+        }
+        
     }
 
     @Override
     public List<U> getFollowedUsers() {
-        return null;
+        final Set<U> users = new HashSet<>();
+        for (final Set<U> group : groups.values()) {
+            users.addAll(group);
+        }
+        return new ArrayList<>(users);
     }
 }
